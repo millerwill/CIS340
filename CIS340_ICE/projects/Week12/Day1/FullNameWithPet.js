ece49ccab8b604a8a9f03a2c84473a089cc4f406
millerwill <wm2705@colostate.edu>
2025-04-08 20:19:44 +0000

    wk12-cont

---
+++ CIS340_ICE/projects/Week12/Day1/FullNameWithPet.js
@@ -0,0 +1,3 @@
+// Import React and necessary components from React Native
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
---
+++ CIS340_ICE/projects/Week12/Day1/PetInfo.js
@@ -0,0 +1,3 @@
+// Importing necessary components from React and React Native
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
