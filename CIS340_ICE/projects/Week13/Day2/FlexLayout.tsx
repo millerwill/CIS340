1fb212b5dc39b39d8987a97126f17e7e89faecfc
millerwill <wm2705@colostate.edu>
2025-04-17 15:14:10 -0600

    Update FlexLayout.tsx

--- CIS340_ICE/projects/Week13/Day2/FlexLayout.tsx
+++ CIS340_ICE/projects/Week13/Day2/FlexLayout.tsx
@@ -5,4 +5,32 @@ import { View } from 'react-native';
 // Main functional component named App
 export default function App() {
 
+    return(
+        <View style = {{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'stretch'
+        }}>
+
+                <View style = {{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: 'red'
+                }}/>
+                        <View style = {{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: 'yellow'
+                }}/>
+        
+                <View style = {{
+                    width: 100,
+                    height: 50,
+                    backgroundColor: 'green'
+                }}/>
+
+    </View>
+    );
+
 }
