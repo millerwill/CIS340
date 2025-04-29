9adbcbfbabbac70b8a9258ddf4b7691c044c8c08
millerwill <wm2705@colostate.edu>
2025-04-29 15:07:59 -0600

    wk15

--- CIS340_ICE/projects/Week15/Day1/CoinFlipApp.js
+++ CIS340_ICE/projects/Week15/Day1/CoinFlipApp.js
@@ -4,6 +4,62 @@ import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
 // Main App component
 export default function App() {
+  const [coinResult, setCoinResult] = useState('Heads');
+
+  const [countdown, setCountdown] = useState(null);
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const images = {     
+    Heads: 'https://raw.githubusercontent.com/AnanthIyerKrishnan/CIS340/main/images/heads.jpg',     
+    Tails: 'https://raw.githubusercontent.com/AnanthIyerKrishnan/CIS340/main/images/tails.jpg',   
+  };
+
+  function flipCoinWithCountdown() {
+    let counter = 3;
+    setCountdown(counter);
+    setIsButtonDisabled(true);
+
+    const countdownInterval = setInterval(() => {
+      counter = counter - 1;
+      setCountdown(counter);
+
+      if(counter === 0){
+        clearInterval(countdownInterval);
+        const result = Math.random() > 0.5 ? 'Heads' : 'Tails';
+        setCoinResult(result);
+        setCountdown(null);
+        setIsButtonDisabled(false);
+      }
+    },500);
+  }
+
+  return (
+    <View style={styles.container}>
+      {countdown === null && (
+        <Text style={styles.resultText}>
+          {coinResult}
+        </Text>
+      )}
+
+      {countdown !== null ? (
+        <Text style={styles.countdownText}>{countdown}</Text>
+      ): (
+        <Image
+          source={{ uri: images[coinResult]}}
+          style={styles.container}
+        />
+      )}
+
+      <View style={styles.buttonWrapper}>
+        <Button
+          title="Flip Coin"
+          onPress={flipCoinWithCountdown}
+          disabled={isButtonDisabled}
+        />
+      </View>
+    </View>
+  );
 }
 
 // Styles for the app
